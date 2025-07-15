b389da13d3c5c3cdaffacc24057f08a8e1e6294d
Qambar Raza <qambar.raza@bbc.co.uk>
2025-07-15 19:17:09 +0100

    Add code for 01_09

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -7,21 +7,12 @@ import org.openqa.selenium.WebElement;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FirstSeleniumTest extends BaseTest {
-	@Test
-	public void checkPageTitle() {
-		driver.get("https://practicesoftwaretesting.com");
-		assertEquals("Practice Software Testing - Toolshop - v5.0", driver.getTitle());
-	}
 
 	@Test
 	public void checkContactHeading() {
 		driver.get("https://practicesoftwaretesting.com/contact");
-
-
-		WebElement heading = driver.findElement(By.tagName("h3"));
-
+		WebElement heading = driver.findElement(By.tagName("h1"));
 		String headingText = heading.getText();
-
 		assertEquals("Contact", headingText);
 
 	}
