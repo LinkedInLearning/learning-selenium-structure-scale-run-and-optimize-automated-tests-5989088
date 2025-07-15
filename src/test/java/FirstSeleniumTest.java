ba8426aff8b4c8b6accea4f348a4c91c0b849b7b
Qambar Raza <qambar.raza@bbc.co.uk>
2025-07-15 19:17:35 +0100

    Add code for 02_04_b

---
+++ src/test/java/FirstSeleniumTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstSeleniumTest extends BaseTest {
+	@Test
+	public void testContactFormSubmit() {
+		driver.get("https://practicesoftwaretesting.com/contact");
+
+		ContactPage contactPagePom = new ContactPage(driver);
+
+		//FillForm
+		contactPagePom.fillForm();
+
+		// Submit
+		contactPagePom.submitForm();
+
+		// Add explicit wait for the success alert to appear
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement alert = wait.until(
+			ExpectedConditions.visibilityOfElementLocated(contactPagePom.alertLoc)
+		);
+		String alertText = alert.getText().trim();
+
+		assertEquals("Thanks for your message! We will contact you shortly.", alertText);
+	}
+}
