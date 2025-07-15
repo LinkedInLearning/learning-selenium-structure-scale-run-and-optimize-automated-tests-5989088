1dc35c713874b574a00cc315ce7057df2f575465
Qambar Raza <qambar.raza@bbc.co.uk>
2025-07-15 19:17:49 +0100

    Add code for 02_06

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
@@ -9,7 +11,7 @@ import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ContactTest extends BaseTest {
+public class FirstSeleniumTest extends BaseTest {
 	@Test
 	public void testContactFormSubmit() {
 		driver.get("https://practicesoftwaretesting.com/contact");
