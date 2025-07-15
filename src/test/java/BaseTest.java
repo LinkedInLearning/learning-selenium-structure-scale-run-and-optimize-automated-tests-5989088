a2bda7c64629da94d8f2cddc03b885766c477fc6
Qambar Raza <qambar.raza@bbc.co.uk>
2025-07-15 19:18:06 +0100

    Add code for 03_03_e

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,23 +5,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BaseTest {
 	protected WebDriver driver;
 
 	@BeforeEach
-	public void setUp() {
-		String browser = System.getProperty("browser", "firefox").toLowerCase();
+	public void setUp() throws MalformedURLException {
+		String browser = System.getProperty("browser", "chrome").toLowerCase();
+		URL gridURL = new URL("http://localhost:4444");
 
 		switch(browser) {
 			case "chrome":
-				driver = new ChromeDriver(new ChromeOptions());
+				driver = new RemoteWebDriver(gridURL, new ChromeOptions());
 				break;
 			case "firefox":
-				driver = new FirefoxDriver(new FirefoxOptions());
+				driver = new RemoteWebDriver(gridURL, new FirefoxOptions());
 				break;
 			default:
-				driver = new ChromeDriver(new ChromeOptions());
+				driver = new RemoteWebDriver(gridURL, new ChromeOptions());
 				break;
 		}
 
